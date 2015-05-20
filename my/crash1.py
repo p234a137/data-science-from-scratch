da72f1b549776f00fe82436b552c83d801660d61
Dorian <p234a137@gmail.com>
2015-05-20 14:54:51 -0700

    simple tests

--- my/crash1.py
+++ my/crash1.py
@@ -86,3 +86,79 @@ one = x[1]
 nine = x[-1] # 'Pythonic' for last element
 eight = x[-2] # 'Pythonic' for next-to-last element
 x[0] = -1 # set first element to -1
+# slicing
+first_three = x[:3]
+three_to_end = x[3:]
+one_to_four = x[1:5]
+last_three = x[-3:]
+without_first_and_last = x[1:-1]
+copy_of_x = x[:]
+# operator to check list membership
+1 in [1, 2, 3] # True
+0 in [1, 2, 3] # False
+# concatenate
+x = [1, 2, 3]
+x.extend([4, 5, 6])
+x = [1, 2, 3]
+y = x + [4, 5, 6]
+x = [1, 2, 3]
+x.append(0)
+y = x[-1] # 0
+z = len(x)
+# list unpacking
+x, y = [1, 2]
+print x, y
+_, y = 1, 2 # _ is a value we throw away
+
+# tuples
+my_list = [1, 2]
+my_tuple = (1, 2)
+other_tuple = 3,4
+try:
+    my_tuple[1] = 3
+except TypeError:
+    print "Cannot modify a tuple"
+# return multiple value from functions using tuples
+def sum_and_product(x, y):
+    return (x+y), (x*y)
+sp = sum_and_product(2, 3)
+s, p = sum_and_product(5, 10)
+x, y = 1, 2 # x=1, y=2
+x, y = y, x # x=2, y=1
+
+# dictionaries
+empty_dict = {} # 'Pythonic' way of initialization
+empty_dict2 = dict() # less Pythonic
+grades = {"Joel" : 80, "Tim" : 95}
+print grades
+#
+joels_grade = grades["Joel"]
+try:
+    kates_grade = grades["Kate"]
+except KeyError:
+    print "no grade for Kate!"
+# check existence of a key
+joel_has_grade = "Joel" in grades # True
+kate_has_grade = "Kate" in grades # False
+# get method with default return value
+joels_grade = grades.get("Joel", 0) # 80
+kates_grade = grades.get("Kate", 0) # 0
+no_ones_grade = grades.get("No One") # default default is None
+print joels_grade, kates_grade, no_ones_grade
+#
+grades["Tim"] = 99 # replaces old values
+grades["Kate"] = 100 # adds a third entry
+num_students = len(grades)
+print grades
+# dictionaries as simple ways to represent structured data
+tweet = {
+    "user" : "joelgrus",
+    "text" : "Data Science is Awesome",
+    "retweet_count" : 100,
+    "hashtags" : ["#data", "#datascience", "#science", "awesome", "#yolo"]
+}
+tweet_keys = tweet.keys()
+tweet_values = tweet.values()
+tweet_items = tweet.items() # list of (key, value) tuples
+
+# defaultdict
